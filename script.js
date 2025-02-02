d62b7caee90a0a9aa9d8d87035393d734793dfa4
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-02 22:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function toCase(text) {
   // write your code here
+	if(str="")
+	{
+		return "-";
+	}
+	let strlowercase=text.toLowerCase();
+	let struppercase=text.toUpperCase();
+	return strlowercase+"-"+struppercase
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
+
+
 alert(toCase(text));
